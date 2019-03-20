10a60670fe03d459ad7064ade14988d65eabef4f
Andrea Panno <31217389+badcortex@users.noreply.github.com>
2019-03-20 18:21:11 +0100

    Script updated!

--- mammography_stats.m
+++ mammography_stats.m
@@ -16,6 +16,9 @@ dataset.Properties.VariableNames{4} = 'Margin';   % Bordi della massa tumorale
 dataset.Properties.VariableNames{5} = 'Density';  % Densita' della massa
 dataset.Properties.VariableNames{6} = 'Severity'; % Benigno / maligno
 
+% Acquisizione del grandezza del dataset originale
+originalDataLength = height(dataset);
+
 % Classificazione delle variabili
 % BIRADS:   ordinale 
 % Age:      rapporti  
@@ -31,7 +34,7 @@ dataset.Properties.VariableNames{6} = 'Severity'; % Benigno / maligno
 % Altro:  Verifica che Shape, Margin, Dnsity e Severity assumano valori
 %         consentiti.
 
-% Eliminazione delle unita' statistica con almeno una variabile che assume
+% Eliminazione delle unita' statistiche con almeno una variabile che assume
 % valori fuori dal dominio dei valori consentiti.
 dataset(dataset.BIRADS == 0 | dataset.BIRADS > 5 | ...
         dataset.Age < 18 | dataset.Age > 90 | ...
@@ -42,8 +45,41 @@ dataset(dataset.BIRADS == 0 | dataset.BIRADS > 5 | ...
 
 % Calcolo dei dati mancanti per ciscuna variabile facendo distinzione tra
 % le categorie diagnostiche.
-
 % Split del dataset in base alle due categorie diagnostiche.
 dBenign = dataset(dataset.Severity == 0,:);
 dMalignant = dataset(dataset.Severity == 1,:);
+
+% Calcolo del numero di unita statistiche valide totali e per ciascuna
+% categoria diagnostica.
+% Il conteggio delle unita statistiche totali valide viene effettuato
+% ricavando la lunghezza del dataset da cui sono state eliminate le 
+% unità non ritenute valide.
+validStatUnits = height(dataset); 
+
+validBiradsB = length(find(~isnan(dBenign.BIRADS)));
+validAgeB = length(find(~isnan(dBenign.Age)));
+validShapeB = length(find(~isnan(dBenign.Shape)));
+validMarginB = length(find(~isnan(dBenign.Margin)));
+validDensityB = length(find(~isnan(dBenign.Density)));
+
+validBiradsM = length(find(~isnan(dMalignant.BIRADS)));
+validAgeM = length(find(~isnan(dMalignant.Age)));
+validShapeM = length(find(~isnan(dMalignant.Shape)));
+validMarginM = length(find(~isnan(dMalignant.Margin)));
+validDensityM = length(find(~isnan(dMalignant.Density)));
+
+% Conteggio dei dati mancanti per ciascuna variabile per le due categorie
+% diagnostiche.
+missingBiradsB = length(find(isnan(dBenign.BIRADS)));
+missingAgeB = length(find(isnan(dBenign.Age)));
+missingShapeB = length(find(isnan(dBenign.Shape)));
+missingMarginB = length(find(isnan(dBenign.Margin)));
+missingDensityB = length(find(isnan(dBenign.Density)));
+
+missingBiradsM = length(find(isnan(dMalignant.BIRADS)));
+missingAgeM = length(find(isnan(dMalignant.Age)));
+missingShapeM = length(find(isnan(dMalignant.Shape)));
+missingMarginM = length(find(isnan(dMalignant.Margin)));
+missingDensityM = length(find(isnan(dMalignant.Density)));
+
 
